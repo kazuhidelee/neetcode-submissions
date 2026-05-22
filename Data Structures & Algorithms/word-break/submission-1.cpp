32a1d61742db79746853982f4698c834f8650172
Tony Lee <122251831+kazuhidelee@users.noreply.github.com>
2026-05-22 16:36:08 -0400

    Add: word-break - submission-1

---
+++ Data Structures & Algorithms/word-break/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<bool> dp(s.length() + 1, 0);
+        dp[0] = true;
+        unordered_set<string> words;
+        for(string s: wordDict){
+            words.insert(s);
+        }
+        for(int i = 1; i < s.length() + 1; ++i){
+            for(int j = 0; j <= i; ++j){
+                if(dp[j] && words.count(s.substr(j, i - j))){
+                    dp[i] = true;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+};
