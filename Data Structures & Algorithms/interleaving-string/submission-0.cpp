b0ce5178e833bcc94a35e179488c0d377478d00b
Tony Lee <122251831+kazuhidelee@users.noreply.github.com>
2026-05-24 00:18:56 -0400

    Add: interleaving-string - submission-0

---
+++ Data Structures & Algorithms/interleaving-string/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        if(s1.length() + s2.length() != s3.length()) return false;
+        vector<vector<bool>> dp(s1.length() + 1, vector<bool>(s2.length() + 1, false));
+        dp[0][0] = true;
+        for(int i = 0; i <= s1.length(); ++i){
+            for(int j = 0; j <= s2.length(); ++j){
+                if(j > 0  && s2[j - 1] == s3[i + j - 1] && dp[i][j - 1]){
+                    dp[i][j] = true;
+                }else if(i > 0 && s1[i - 1] == s3[i + j - 1] && dp[i - 1][j]){
+                    dp[i][j] = true;
+                }
+            }
+        }
+        return dp[s1.length()][s2.length()];
+    }
+};
