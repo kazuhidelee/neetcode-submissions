63b23bf47523d2595045e368e47642d0bdaff258
Tony Lee <122251831+kazuhidelee@users.noreply.github.com>
2026-05-31 17:26:06 -0400

    Add: multiply-strings - submission-4

---
+++ Data Structures & Algorithms/multiply-strings/submission-4.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        vector<int> v1;
+        vector<int> v2;
+        for(int i = num1.length() - 1; i >=0 ; --i){
+            v1.push_back(num1[i] - '0');
+        }
+        for(int i = num2.length()- 1; i >=0 ; --i){
+            v2.push_back(num2[i] - '0');
+        }
+        vector<int> summing(num1.length() + num2.length(), 0);
+        for(int i = 0; i < v2.size(); ++i){
+            for(int j = 0; j < v1.size(); ++j){
+                int product = v1[j] * v2[i];
+                summing[i + j] += product;
+            }
+            for(int j = i; j < i + v1.size(); ++j){
+                int carry = summing[j] / 10;
+                int remain = summing[j] % 10;
+                summing[j] = remain;
+                summing[j + 1] += carry;
+            }
+        }
+        string res = "";
+        for(int i = summing.size() - 1; i >= 0; --i){
+            if(res == "" && summing[i] == 0){
+                continue;
+            }else{
+                res += char('0' + summing[i]);
+            }
+        }
+        if(res == "") return "0";
+        return res;
+     
+    }
+};
