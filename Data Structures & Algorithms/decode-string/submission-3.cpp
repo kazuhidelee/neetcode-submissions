6ba95be337fdb6e493d1cb337ac522e8ad93c6e8
Tony Lee <122251831+kazuhidelee@users.noreply.github.com>
2026-06-18 16:01:20 -0400

    Add: decode-string - submission-3

---
+++ Data Structures & Algorithms/decode-string/submission-3.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        string res = "";
+        stack<string> encoded;
+        stack<int> nums;
+        string curr = "";
+        string n = "";
+        for(char c: s){
+            if(isdigit(c)){
+                n += c;
+            }else if(c == '['){
+                nums.push(stoi(n));
+                encoded.push(curr);
+                curr = "";
+                n = "";
+            }else if(c == ']'){
+                string temp = "";
+                int n = nums.top();
+                while(n--) temp += curr;
+                nums.pop();
+                if(!encoded.empty()){
+                    string t = encoded.top();
+                    temp = t + temp;
+                    encoded.pop();
+                }
+
+                curr = temp;
+            }else{
+                // normal letter
+                curr += c;
+            }
+        }
+        if(curr != ""){
+            encoded.push(curr);
+        }
+
+        while(!encoded.empty()){
+            res = encoded.top() + res;
+            encoded.pop();
+        }
+
+        return res;
+    
+    }
+};
