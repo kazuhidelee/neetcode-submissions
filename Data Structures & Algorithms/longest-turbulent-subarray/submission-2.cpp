e44aebd023cac28774940e3488ae46ccbe5f0004
Tony Lee <122251831+kazuhidelee@users.noreply.github.com>
2026-08-26 00:30:39 -0400

    Add: longest-turbulent-subarray - submission-2

---
+++ Data Structures & Algorithms/longest-turbulent-subarray/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxTurbulenceSize(vector<int>& arr) {
+        if(arr.size() == 1) return 1;
+        
+        if(arr.size() == 2){
+            if(arr[0] == arr[1]) return 1;
+            return 2;
+        } 
+        int comparison = arr[1] - arr[0];
+        int max_sub = 1;
+        int l = 0;
+        for(int r = 2; r < arr.size(); ++r){
+            if(!(arr[r] > arr[r - 1] && comparison < 0 || arr[r] < arr[r - 1] && comparison > 0)){
+               if(arr[r] == arr[r - 1]){
+                l = r;
+               }else{   
+                l = r - 1;
+               }
+            }
+            max_sub = max(max_sub, r - l + 1);
+            comparison = arr[r] - arr[r - 1];
+        }
+        return max_sub;
+    }
+};
